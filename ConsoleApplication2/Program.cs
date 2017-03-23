68afdf9ae399758e776581bcfc35ec266c941a7b
berouq <b>
2017-03-23 14:58:35 -0400

    Revert "making a change to program.cs for only hte master branch, to diverge from add writeline brach"
    This reverts commit 14c5735176a074885c6bce91ecb17bcb61027526.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("hi everyone!!!");
-            Console.WriteLine("this is a change only for the master branch");
         }
     }
 }
